5fbbbc29eb754d658f24323933803a89d9c1e27d
Ahsan Al Rupom <ahsanal.rupom@scicom.com.my>
2017-05-03 18:52:09 +0800

    Added Passport Middleware

--- app.js
+++ app.js
@@ -1,23 +1,41 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+// var passport = require('passport');
+var session = require('express-session');
 
 var app = express();
 var port = process.env.PORT || 3000;
 
-var nav= [{
-                link: '/books',
-                text: 'Book'
-            }, {
-                link: 'authors',
-                text: 'Author'
-            }];
+var nav = [{
+    link: '/books',
+    text: 'Book'
+}, {
+    link: 'authors',
+    text: 'Author'
+}];
 var bookRouter = require('./src/routes/bookRoutes')(nav);
 var adminRouter = require('./src/routes/adminRoutes')(nav);
+var authRouter = require('./src/routes/authRoutes');
+
 app.use(express.static('public'));
+app.use(bodyParser.json());
+/*app.use(bodyParser.urlencoded({
+    extended: false
+}));*/
+app.use(cookieParser());
+app.use(session({
+    secret: 'library'
+}));
+
+require('./src/config/passport')(app);
+
 app.set('views', 'src/views');
 app.set('view engine', 'ejs');
 
 app.use('/books', bookRouter);
-app.use('/Admin', adminRouter);
+app.use('/auth', authRouter);
+app.use('/admin', adminRouter);
 
 app.get('/', function (req, res) {
     res.render('index', {
